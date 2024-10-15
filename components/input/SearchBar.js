a82a97a198dd4830fb64b1d6b17c103ab8b0671c
TimmyGuy <tim.ohlsen2@gmail.com>
2024-10-15 17:54:23 +0200

    Autofocus search field

--- components/input/SearchBar.js
+++ components/input/SearchBar.js
@@ -50,6 +50,7 @@ export default function SearchBar() {
                     className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-10 md:pr-20 sm:text-sm border-gray-300 rounded-md"
                     placeholder="Search a movie title"
                     onChange={searchSuggestions}
+                    autoFocus={true}
                 />
                 <div className="md:block absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
                     <kbd className="inline-flex items-center border border-gray-200 rounded px-2 text-sm font-sans font-medium text-gray-400">
